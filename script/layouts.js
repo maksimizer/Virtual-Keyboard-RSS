f543f1921310ff15af56d6445a9e5d1daa19d4dc
maksimizer <maximorlov@mail.ru>
2022-05-07 15:07:21 +0300

    feat: create layouts module with keysObj

---
+++ script/layouts.js
@@ -0,0 +1,899 @@
+const keysObj = {
+  tilda: {
+    en: {
+      low: '`',
+      high: '~',
+      capsOn: '`',
+      capsOnshift: '~',
+    },
+    ru: {
+      low: 'ё',
+      high: 'Ё',
+      capsOn: 'Ё',
+      capsOnshift: 'ё',
+    },
+  },
+  Num1: {
+    en: {
+      low: '1',
+      high: '!',
+      capsOn: '1',
+      capsOnshift: '!',
+    },
+    ru: {
+      low: '1',
+      high: '!',
+      capsOn: '1',
+      capsOnshift: '!',
+    },
+  },
+  Num2: {
+    en: {
+      low: '2',
+      high: '@',
+      capsOn: '2',
+      capsOnshift: '@',
+    },
+    ru: {
+      low: '2',
+      high: '""',
+      capsOn: '2',
+      capsOnshift: '""',
+    },
+  },
+  Num3: {
+    en: {
+      low: '3',
+      high: '#',
+      capsOn: '3',
+      capsOnshift: '#',
+    },
+    ru: {
+      low: '3',
+      high: '№',
+      capsOn: '3',
+      capsOnshift: '№',
+    },
+  },
+  Num4: {
+    en: {
+      low: '4',
+      high: '$',
+      capsOn: '4',
+      capsOnshift: '$',
+    },
+    ru: {
+      low: '4',
+      high: ';',
+      capsOn: '4',
+      capsOnshift: ';',
+    },
+  },
+  Num5: {
+    en: {
+      low: '5',
+      high: '%',
+      capsOn: '5',
+      capsOnshift: '%',
+    },
+    ru: {
+      low: '5',
+      high: '%',
+      capsOn: '5',
+      capsOnshift: '%',
+    },
+  },
+  Num6: {
+    en: {
+      low: '6',
+      high: '^',
+      capsOn: '6',
+      capsOnshift: '^',
+    },
+    ru: {
+      low: '6',
+      high: ':',
+      capsOn: '6',
+      capsOnshift: ':',
+    },
+  },
+  Num7: {
+    en: {
+      low: '7',
+      high: '&',
+      capsOn: '7',
+      capsOnshift: '&',
+    },
+    ru: {
+      low: '7',
+      high: '?',
+      capsOn: '7',
+      capsOnshift: '?',
+    },
+  },
+  Num8: {
+    en: {
+      low: '8',
+      high: '*',
+      capsOn: '8',
+      capsOnshift: '*',
+    },
+    ru: {
+      low: '8',
+      high: '*',
+      capsOn: '8',
+      capsOnshift: '*',
+    },
+  },
+  Num9: {
+    en: {
+      low: '9',
+      high: '(',
+      capsOn: '9',
+      capsOnshift: '(',
+    },
+    ru: {
+      low: '9',
+      high: '(',
+      capsOn: '9',
+      capsOnshift: '(',
+    },
+  },
+  Num0: {
+    en: {
+      low: '0',
+      high: ')',
+      capsOn: '0',
+      capsOnshift: ')',
+    },
+    ru: {
+      low: '0',
+      high: ')',
+      capsOn: '0',
+      capsOnshift: ')',
+    },
+  },
+  minus: {
+    en: {
+      low: '-',
+      high: '_',
+      capsOn: '-',
+      capsOnshift: '_',
+    },
+    ru: {
+      low: '-',
+      high: '_',
+      capsOn: '-',
+      capsOnshift: '_',
+    },
+  },
+  equal: {
+    en: {
+      low: '=',
+      high: '+',
+      capsOn: '=',
+      capsOnshift: '+',
+    },
+    ru: {
+      low: '=',
+      high: '+',
+      capsOn: '=',
+      capsOnshift: '+',
+    },
+  },
+  backspace: {
+    en: {
+      low: 'backspace',
+      high: 'backspace',
+      capsOn: 'backspace',
+      capsOnshift: 'backspace',
+    },
+    ru: {
+      low: 'backspace',
+      high: 'backspace',
+      capsOn: 'backspace',
+      capsOnshift: 'backspace',
+    },
+  },
+  tab: {
+    en: {
+      low: 'tab',
+      high: 'tab',
+      capsOn: 'tab',
+      capsOnshift: 'tab',
+    },
+    ru: {
+      low: 'tab',
+      high: 'tab',
+      capsOn: 'tab',
+      capsOnshift: 'tab',
+    },
+  },
+  Q: {
+    en: {
+      low: 'q',
+      high: 'Q',
+      capsOn: 'Q',
+      capsOnshift: 'q',
+    },
+    ru: {
+      low: 'й',
+      high: 'Й',
+      capsOn: 'Й',
+      capsOnshift: 'й',
+    },
+  },
+  W: {
+    en: {
+      low: 'w',
+      high: 'W',
+      capsOn: 'W',
+      capsOnshift: 'w',
+    },
+    ru: {
+      low: 'ц',
+      high: 'Ц',
+      capsOn: 'Ц',
+      capsOnshift: 'ц',
+    },
+  },
+  E: {
+    en: {
+      low: 'e',
+      high: 'E',
+      capsOn: 'E',
+      capsOnshift: 'e',
+    },
+    ru: {
+      low: 'у',
+      high: 'У',
+      capsOn: 'У',
+      capsOnshift: 'у',
+    },
+  },
+  R: {
+    en: {
+      low: 'r',
+      high: 'R',
+      capsOn: 'R',
+      capsOnshift: 'r',
+    },
+    ru: {
+      low: 'к',
+      high: 'К',
+      capsOn: 'К',
+      capsOnshift: 'к',
+    },
+  },
+  T: {
+    en: {
+      low: 't',
+      high: 'T',
+      capsOn: 'T',
+      capsOnshift: 't',
+    },
+    ru: {
+      low: 'е',
+      high: 'Е',
+      capsOn: 'Е',
+      capsOnshift: 'е',
+    },
+  },
+  Y: {
+    en: {
+      low: 'y',
+      high: 'Y',
+      capsOn: 'Y',
+      capsOnshift: 'y',
+    },
+    ru: {
+      low: 'н',
+      high: 'Н',
+      capsOn: 'Н',
+      capsOnshift: 'н',
+    },
+  },
+  U: {
+    en: {
+      low: 'u',
+      high: 'U',
+      capsOn: 'U',
+      capsOnshift: 'u',
+    },
+    ru: {
+      low: 'г',
+      high: 'Г',
+      capsOn: 'Г',
+      capsOnshift: 'г',
+    },
+  },
+  I: {
+    en: {
+      low: 'i',
+      high: 'I',
+      capsOn: 'I',
+      capsOnshift: 'i',
+    },
+    ru: {
+      low: 'ш',
+      high: 'Ш',
+      capsOn: 'Ш',
+      capsOnshift: 'ш',
+    },
+  },
+  O: {
+    en: {
+      low: 'o',
+      high: 'O',
+      capsOn: 'O',
+      capsOnshift: 'o',
+    },
+    ru: {
+      low: 'щ',
+      high: 'Щ',
+      capsOn: 'Щ',
+      capsOnshift: 'щ',
+    },
+  },
+  P: {
+    en: {
+      low: 'p',
+      high: 'P',
+      capsOn: 'P',
+      capsOnshift: 'p',
+    },
+    ru: {
+      low: 'з',
+      high: 'З',
+      capsOn: 'З',
+      capsOnshift: 'з',
+    },
+  },
+  leftFigBracket: {
+    en: {
+      low: '[',
+      high: '{',
+      capsOn: '[',
+      capsOnshift: '{',
+    },
+    ru: {
+      low: 'х',
+      high: 'Х',
+      capsOn: 'Х',
+      capsOnshift: 'х',
+    },
+  },
+  rightFigBracket: {
+    en: {
+      low: ']',
+      high: '}',
+      capsOn: ']',
+      capsOnshift: '}',
+    },
+    ru: {
+      low: 'ъ',
+      high: 'Ъ',
+      capsOn: 'Ъ',
+      capsOnshift: 'ъ',
+    },
+  },
+  backslash: {
+    en: {
+      low: '\\',
+      high: '|',
+      capsOn: '\\',
+      capsOnshift: '|',
+    },
+    ru: {
+      low: '\\',
+      high: '|',
+      capsOn: '\\',
+      capsOnshift: '|',
+    },
+  },
+  delete: {
+    en: {
+      low: 'Del',
+      high: 'Del',
+      capsOn: 'Del',
+      capsOnshift: 'Del',
+    },
+    ru: {
+      low: 'Del',
+      high: 'Del',
+      capsOn: 'Del',
+      capsOnshift: 'Del',
+    },
+  },
+  capsLock: {
+    en: {
+      low: 'caps lock',
+      high: 'caps lock',
+      capsOn: 'caps lock',
+      capsOnshift: 'caps lock',
+    },
+    ru: {
+      low: 'caps lock',
+      high: 'caps lock',
+      capsOn: 'caps lock',
+      capsOnshift: 'caps lock',
+    },
+  },
+  A: {
+    en: {
+      low: 'a',
+      high: 'A',
+      capsOn: 'A',
+      capsOnshift: 'a',
+    },
+    ru: {
+      low: 'ф',
+      high: 'Ф',
+      capsOn: 'Ф',
+      capsOnshift: 'ф',
+    },
+  },
+  S: {
+    en: {
+      low: 's',
+      high: 'S',
+      capsOn: 'S',
+      capsOnshift: 's',
+    },
+    ru: {
+      low: 'ы',
+      high: 'Ы',
+      capsOn: 'Ы',
+      capsOnshift: 'ы',
+    },
+  },
+  D: {
+    en: {
+      low: 'd',
+      high: 'D',
+      capsOn: 'D',
+      capsOnshift: 'd',
+    },
+    ru: {
+      low: 'в',
+      high: 'В',
+      capsOn: 'В',
+      capsOnshift: 'в',
+    },
+  },
+  F: {
+    en: {
+      low: 'f',
+      high: 'F',
+      capsOn: 'F',
+      capsOnshift: 'f',
+    },
+    ru: {
+      low: 'а',
+      high: 'А',
+      capsOn: 'А',
+      capsOnshift: 'а',
+    },
+  },
+  G: {
+    en: {
+      low: 'g',
+      high: 'G',
+      capsOn: 'G',
+      capsOnshift: 'g',
+    },
+    ru: {
+      low: 'п',
+      high: 'П',
+      capsOn: 'П',
+      capsOnshift: 'п',
+    },
+  },
+  H: {
+    en: {
+      low: 'h',
+      high: 'H',
+      capsOn: 'H',
+      capsOnshift: 'h',
+    },
+    ru: {
+      low: 'р',
+      high: 'Р',
+      capsOn: 'Р',
+      capsOnshift: 'р',
+    },
+  },
+  J: {
+    en: {
+      low: 'j',
+      high: 'J',
+      capsOn: 'J',
+      capsOnshift: 'j',
+    },
+    ru: {
+      low: 'о',
+      high: 'О',
+      capsOn: 'О',
+      capsOnshift: 'о',
+    },
+  },
+  K: {
+    en: {
+      low: 'k',
+      high: 'K',
+      capsOn: 'K',
+      capsOnshift: 'k',
+    },
+    ru: {
+      low: 'л',
+      high: 'Л',
+      capsOn: 'Л',
+      capsOnshift: 'л',
+    },
+  },
+  L: {
+    en: {
+      low: 'l',
+      high: 'L',
+      capsOn: 'L',
+      capsOnshift: 'l',
+    },
+    ru: {
+      low: 'д',
+      high: 'Д',
+      capsOn: 'Д',
+      capsOnshift: 'д',
+    },
+  },
+  colon: {
+    en: {
+      low: ';',
+      high: ':',
+      capsOn: ';',
+      capsOnshift: ':',
+    },
+    ru: {
+      low: 'ж',
+      high: 'Ж',
+      capsOn: 'Ж',
+      capsOnshift: 'ж',
+    },
+  },
+  quote: {
+    en: {
+      low: "'",
+      high: '"',
+      capsOn: "'",
+      capsOnshift: '"',
+    },
+    ru: {
+      low: 'э',
+      high: 'Э',
+      capsOn: 'Э',
+      capsOnshift: 'э',
+    },
+  },
+  enter: {
+    en: {
+      low: 'enter',
+      high: 'enter',
+      capsOn: 'enter',
+      capsOnshift: 'enter',
+    },
+    ru: {
+      low: 'enter',
+      high: 'enter',
+      capsOn: 'enter',
+      capsOnshift: 'enter',
+    },
+  },
+  shiftL: {
+    en: {
+      low: 'shift',
+      high: 'shift',
+      capsOn: 'shift',
+      capsOnshift: 'shift',
+    },
+    ru: {
+      low: 'shift',
+      high: 'shift',
+      capsOn: 'shift',
+      capsOnshift: 'shift',
+    },
+  },
+  Z: {
+    en: {
+      low: 'z',
+      high: 'Z',
+      capsOn: 'Z',
+      capsOnshift: 'z',
+    },
+    ru: {
+      low: 'я',
+      high: 'Я',
+      capsOn: 'Я',
+      capsOnshift: 'Я',
+    },
+  },
+  X: {
+    en: {
+      low: 'x',
+      high: 'X',
+      capsOn: 'X',
+      capsOnshift: 'x',
+    },
+    ru: {
+      low: 'ч',
+      high: 'Ч',
+      capsOn: 'Ч',
+      capsOnshift: 'ч',
+    },
+  },
+  C: {
+    en: {
+      low: 'c',
+      high: 'C',
+      capsOn: 'C',
+      capsOnshift: 'c',
+    },
+    ru: {
+      low: 'с',
+      high: 'С',
+      capsOn: 'С',
+      capsOnshift: 'с',
+    },
+  },
+  V: {
+    en: {
+      low: 'v',
+      high: 'V',
+      capsOn: 'V',
+      capsOnshift: 'v',
+    },
+    ru: {
+      low: 'м',
+      high: 'М',
+      capsOn: 'М',
+      capsOnshift: 'м',
+    },
+  },
+  B: {
+    en: {
+      low: 'b',
+      high: 'B',
+      capsOn: 'B',
+      capsOnshift: 'b',
+    },
+    ru: {
+      low: 'и',
+      high: 'И',
+      capsOn: 'И',
+      capsOnshift: 'и',
+    },
+  },
+  N: {
+    en: {
+      low: 'n',
+      high: 'N',
+      capsOn: 'N',
+      capsOnshift: 'n',
+    },
+    ru: {
+      low: 'т',
+      high: 'Т',
+      capsOn: 'Т',
+      capsOnshift: 'т',
+    },
+  },
+  M: {
+    en: {
+      low: 'm',
+      high: 'M',
+      capsOn: 'M',
+      capsOnshift: 'm',
+    },
+    ru: {
+      low: 'о',
+      high: 'О',
+      capsOn: 'О',
+      capsOnshift: 'о',
+    },
+  },
+  comma: {
+    en: {
+      low: ',',
+      high: '<',
+      capsOn: ',',
+      capsOnshift: '<',
+    },
+    ru: {
+      low: 'б',
+      high: 'Б',
+      capsOn: 'Б',
+      capsOnshift: 'б',
+    },
+  },
+  dot: {
+    en: {
+      low: '.',
+      high: '>',
+      capsOn: '.',
+      capsOnshift: '>',
+    },
+    ru: {
+      low: 'ю',
+      high: 'Ю',
+      capsOn: 'Ю',
+      capsOnshift: 'ю',
+    },
+  },
+  slash: {
+    en: {
+      low: '/',
+      high: '?',
+      capsOn: '/',
+      capsOnshift: '?',
+    },
+    ru: {
+      low: '.',
+      high: ',',
+      capsOn: '.',
+      capsOnshift: ',',
+    },
+  },
+  up: {
+    en: {
+      low: '↑',
+      high: '↑',
+      capsOn: '↑',
+      capsOnshift: '↑',
+    },
+    ru: {
+      low: '↑',
+      high: '↑',
+      capsOn: '↑',
+      capsOnshift: '↑',
+    },
+  },
+  shiftR: {
+    en: {
+      low: 'shift',
+      high: 'shift',
+      capsOn: 'shift',
+      capsOnshift: 'shift',
+    },
+    ru: {
+      low: 'shift',
+      high: 'shift',
+      capsOn: 'shift',
+      capsOnshift: 'shift',
+    },
+  },
+  ctrlL: {
+    en: {
+      low: 'ctrl',
+      high: 'ctrl',
+      capsOn: 'ctrl',
+      capsOnshift: 'ctrl',
+    },
+    ru: {
+      low: 'ctrl',
+      high: 'ctrl',
+      capsOn: 'ctrl',
+      capsOnshift: 'ctrl',
+    },
+  },
+  win: {
+    en: {
+      low: 'win',
+      high: 'win',
+      capsOn: 'win',
+      capsOnshift: 'win',
+    },
+    ru: {
+      low: 'win',
+      high: 'win',
+      capsOn: 'win',
+      capsOnshift: 'win',
+    },
+  },
+  altL: {
+    en: {
+      low: 'alt',
+      high: 'alt',
+      capsOn: 'alt',
+      capsOnshift: 'alt',
+    },
+    ru: {
+      low: 'alt',
+      high: 'alt',
+      capsOn: 'alt',
+      capsOnshift: 'alt',
+    },
+  },
+  space: {
+    en: {
+      low: ' ',
+      high: ' ',
+      capsOn: ' ',
+      capsOnshift: ' ',
+    },
+    ru: {
+      low: ' ',
+      high: ' ',
+      capsOn: ' ',
+      capsOnshift: ' ',
+    },
+  },
+  altR: {
+    en: {
+      low: 'alt',
+      high: 'alt',
+      capsOn: 'alt',
+      capsOnshift: 'alt',
+    },
+    ru: {
+      low: 'alt',
+      high: 'alt',
+      capsOn: 'alt',
+      capsOnshift: 'alt',
+    },
+  },
+  left: {
+    en: {
+      low: '←',
+      high: '←',
+      capsOn: '←',
+      capsOnshift: '←',
+    },
+    ru: {
+      low: '←',
+      high: '←',
+      capsOn: '←',
+      capsOnshift: '←',
+    },
+  },
+  down: {
+    en: {
+      low: '↓',
+      high: '↓',
+      capsOn: '↓',
+      capsOnshift: '↓',
+    },
+    ru: {
+      low: '↓',
+      high: '↓',
+      capsOn: '↓',
+      capsOnshift: '↓',
+    },
+  },
+  right: {
+    en: {
+      low: '→',
+      high: '→',
+      capsOn: '→',
+      capsOnshift: '→',
+    },
+    ru: {
+      low: '→',
+      high: '→',
+      capsOn: '→',
+      capsOnshift: '→',
+    },
+  },
+  ctrlR: {
+    en: {
+      low: 'ctrl',
+      high: 'ctrl',
+      capsOn: 'ctrl',
+      capsOnshift: 'ctrl',
+    },
+    ru: {
+      low: 'ctrl',
+      high: 'ctrl',
+      capsOn: 'ctrl',
+      capsOnshift: 'ctrl',
+    },
+  },
+};
+export default keysObj;
