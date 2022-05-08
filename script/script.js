46da9a2af336bdb726a67e100db43b96fe6ead69
maksimizer <maximorlov@mail.ru>
2022-05-08 13:52:41 +0300

    feat: add init function

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 import keysObj from './layouts.js';
 
-const layout = 'ru';
+const layout = localStorage.getItem('layout') || 'en';
 const isShiftOn = false;
 const isCapsOn = false;
 
@@ -29,7 +29,46 @@ function getKeysArr() { //= =============================GETTING DIVS
 }
 const keysArr = getKeysArr();
 
+function init() {
+  const wrapper = document.createElement('div'); //=================PAGE CONTAINER
+  wrapper.classList.add('wrapper');
 
+  const title = document.createElement('h1');  //====================title
+  title.textContent = 'Virtual Keyboard for Windows OS';
+  title.classList.add('title');
+
+  const layoutInfo = document.createElement('p'); //=============Current layout info
+  layoutInfo.classList.add('info');
+  let currentLayout = layout;
+  currentLayout === 'en'?
+  layoutInfo.textContent = `Current layout: ${layout}. Press Left Shift + Left Alt keys to change layout.` :
+  layoutInfo.textContent = `Текущая раскладка: ${layout}. Нажмите левые Shift + Alt чтобы сменить раскладку.`;
+  
+  const textarea = document.createElement('textarea'); //=========textarea
+  textarea.classList.add('textarea');
+
+  const keyLineArr = [];   //=====================key lines
+  for (let i = 0; i < 5; i++) {
+    const keyLine = document.createElement('div');
+    keyLineArr.push(keyLine);
+  };
+    keyLineArr.forEach(line => line.classList.add('key-line'));
+  
+  const keyboard = document.createElement('div'); //===============keyboard
+  keyboard.classList.add('keyboard');
+
+  for (let i = 0; i < keysArr.length; i++) {  //===============putting keys into lines
+    if (i <= 13) keyLineArr[0].append(keysArr[i]);
+    if (i >= 14 && i <= 28) keyLineArr[1].append(keysArr[i]);
+    if (i >= 29 && i <= 41) keyLineArr[2].append(keysArr[i]);
+    if (i >= 42 && i <= 54) keyLineArr[3].append(keysArr[i]);
+    if (i >= 55) keyLineArr[4].append(keysArr[i]);
+  };
+  
+  keyboard.append(...keyLineArr);  //==========putting lines to keyboard
+  wrapper.append(title, layoutInfo, textarea, keyboard); //====putting content to wrapper
+  document.body.prepend(wrapper); //=====putting wraper to body before script tag
+};
 
 window.addEventListener('DOMContentLoaded', init);
 window.addEventListener('beforeunload', () => localStorage.setItem('layout', layout));
